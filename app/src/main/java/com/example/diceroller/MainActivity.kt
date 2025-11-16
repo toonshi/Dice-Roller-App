c67d3e8302766a54a568a4f14e3210f13f2fd1ef
Mike Agoya <agoyamike@gmail.com>
2025-11-17 00:55:00 +0300

    create layout infastructure, create vertical layout, add a button

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.diceroller.ui.theme.DiceRollerTheme
+
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DiceRollerTheme {
+                DiceRollerApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun DiceWithButtonAndImage (modifier: Modifier = Modifier){
+Column(
+    modifier = modifier,
+    horizontalAlignment = Alignment.CenterHorizontally
+
+){
+  Button(onClick = {/*TODo*/}){
+   Text(stringResource(R.string.roll))
+  }
+}
+}
+
+@Preview
+@Composable
+fun DiceRollerApp(){
+    DiceWithButtonAndImage(modifier = Modifier.fillMaxSize().wrapContentSize(Alignment.Center))
+}
+
